c8f180affc5fcda881c2e35e626783a67df1f27e
vaidasuope <vaida.dabasinskaite@gmail.com>
2021-02-21 14:32:35 +0200

    added db config

--- resources/views/_partials/head.blade.php
+++ resources/views/_partials/head.blade.php
@@ -15,5 +15,5 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{secure_asset('css/app.css') }}" rel="stylesheet">
 </head>
