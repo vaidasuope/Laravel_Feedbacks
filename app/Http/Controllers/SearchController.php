057c69a3da73d31cbba0f9cd3b8c2a0d9a1e2c5b
vaidasuope <vaida.dabasinskaite@gmail.com>
2021-02-21 16:33:47 +0200

    added db config

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -35,7 +35,7 @@ class SearchController extends Controller
             ->select('services.id', 'services.gender', 'services.first_name', 'services.last_name',
                 'specializations.specialization_name', 'companies.company_name', 'services.city', 'services.img',
                 DB::raw('ROUND(AVG(reviews.stars)) as stars'), DB::raw('COUNT(reviews.stars) as number'))
-            ->groupBy('services.id','specializations.specialization_name','companies.company_name' );
+            ->groupBy('services.id','specializations.specialization_name','companies.company_name');
 
 
         if ($request->filled('specialization_name')) {
@@ -61,7 +61,7 @@ class SearchController extends Controller
                 ->orWhere('company_name','LIKE', '%' . $request->search . '%');
         }
 
-        return view('pages/search', ['services' => $services->paginate(3)], compact('specializations', 'companies',
+        return view('pages/search', ['services' => $services->get()], compact('specializations', 'companies',
             'genders', 'cities'));
     }
 }
